fa9e21d4d3e58ef521424de898bf24d0bc4f0ed0
Josue Valbuena <joshvlbn@gmail.com>
2023-06-11 21:04:53 -0400

    desafio terminado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, console.log('servidor iniciado en el puerto '+ port));
+app.listen(port, console.log('servidor iniciado en el puerto ' + port));
 
 const cors = require('cors');
 const fs = require('fs');
@@ -10,7 +10,42 @@ const fs = require('fs');
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+//mostrar canciones
+app.get('/canciones', (req, res) => {
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
+    res.send(canciones);
+})
+
+//agregar canciones
+app.post('/canciones', (req, res) => {
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
+    canciones.push(cancion);
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('cancion agregada correctamente')
+})
+
+//eliminar canciones
+app.delete('/canciones/:id', (req, res) => {
+    const id = req.params.id;
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
+    const index = canciones.findIndex(ele => ele.id == id);
+    canciones.splice(index, 1);
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('cancion eliminada correctamente')
+})
+
+//editar canciones
+app.put('/canciones/:id', (req, res) => {
+    const id = req.params.id;
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'));
+    const index = canciones.findIndex(ele => ele.id == id);
+    canciones[index] = cancion;
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones));
+    res.send('cancion editada correctamente');
+})
